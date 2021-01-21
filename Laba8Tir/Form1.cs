5e7ba96715acc15b19038785c7ff4cf86a58dd22
ketaterina <73121693+ketaterina@users.noreply.github.com>
2021-01-21 20:07:38 +0500

    Add files via upload

---
+++ Laba8Tir/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba8Tir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(255, 128, 128);
+            this.ResumeLayout(false);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics graphic = CreateGraphics();
+            SolidBrush brush = new SolidBrush(Color.FromArgb(255, 128, 128));
+
+            graphic.FillEllipse(brush, new Rectangle(e.X - 50, e.Y - 50, 100, 100));
+        }
+    }
+}
